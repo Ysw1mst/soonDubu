9fb57934799a6dae268dfd8bda02fb7836227d25
Ysw1mst_Mac <ysw1mst@Ysw1mst-Macui-MacBookPro.local>
2024-10-31 19:40:01 +0900

    Your commit message

--- static/script.js
+++ static/script.js
@@ -1,6 +1,9 @@
-let numButtonClicks = 0;
-function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
-}
+document.getElementById("resetButton").addEventListener("click", function() {
+    // 파일 선택 초기화
+    document.querySelector('input[type="file"]').value = ""; 
+    // 변환된 MIDI 파일 다운로드 링크 초기화
+    const downloadLink = document.querySelector('a[href*="static"]');
+    if (downloadLink) {
+        downloadLink.parentNode.removeChild(downloadLink); // 링크 제거
+    }
+});
