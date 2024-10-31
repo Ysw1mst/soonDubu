f4a498127d2cb9f062b6c31fff3e8167a255cd7d
Ysw1mst_Mac <ysw1mst@Ysw1mst-Macui-MacBookPro.local>
2024-10-31 20:07:37 +0900

    Your commit message

--- static/script.js
+++ static/script.js
@@ -1,40 +1,27 @@
-document.getElementById("resetButton").addEventListener("click", function() {
-    // 파일 선택 초기화
-    document.querySelector('input[type="file"]').value = ""; 
-    // 변환된 MIDI 파일 다운로드 링크 초기화
-    const downloadLink = document.querySelector('a[href*="static"]');
-    if (downloadLink) {
-        downloadLink.parentNode.removeChild(downloadLink); // 링크 제거
-    }
-    // 업로드 상태 초기화 메시지 추가
-    alert("초기화되었습니다.");
-});
+const resizers = document.querySelectorAll('.resizer');
+const boxes = document.querySelectorAll('.box');
 
-// 구분선 조정 기능 추가
-const dividers = document.querySelectorAll('.divider');
-dividers.forEach(divider => {
-    divider.addEventListener('mousedown', initResize);
-});
+resizers.forEach((resizer, index) => {
+    let startX;
+    let startWidth;
 
-function initResize(e) {
-    window.addEventListener('mousemove', Resize);
-    window.addEventListener('mouseup', stopResize);
-}
+    resizer.addEventListener('mousedown', (e) => {
+        startX = e.clientX;
+        startWidth = boxes[index].offsetWidth;
 
-function Resize(e) {
-    const leftBox = e.target.previousElementSibling;
-    const rightBox = e.target.nextElementSibling;
+        document.addEventListener('mousemove', resize);
+        document.addEventListener('mouseup', stopResize);
+    });
 
-    if (leftBox && rightBox) {
-        const newWidth = e.clientX - leftBox.getBoundingClientRect().left; // 마우스의 X 위치
-        if (newWidth > 0) { // 너비가 0보다 클 때
-            leftBox.style.width = `${newWidth}px`;
-            rightBox.style.width = `${window.innerWidth - newWidth - 50}px`; // 50px는 두 박스와 구분선의 총 너비
+    function resize(e) {
+        const newWidth = startWidth + (e.clientX - startX);
+        if (newWidth > 100) { // 최소 너비 제한
+            boxes[index].style.width = newWidth + 'px';
         }
     }
-}
 
-function stopResize() {
-    window.removeEventListener('mousemove', Resize);
-    window.removeEventListener('mouseup', stopResize);
-}
+    function stopResize() {
+        document.removeEventListener('mousemove', resize);
+        document.removeEventListener('mouseup', stopResize);
+    }
+});
